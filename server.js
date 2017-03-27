692c34ec6a35cdbd946cd037494b767a404526aa
lehoangvu <hoangvu171819@gmail.com>
2017-03-27 10:38:23 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.use(
     express.static(__dirname) //where your static content is located in your filesystem
 );
 
-app.listen(80); //the port you want to use
+app.listen(8080); //the port you want to use
